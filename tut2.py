01983215a5a5c303d59b13e1f2f3cdd21034af52
Salma7577 <salman757@outlook.sa>
2022-07-26 01:20:46 +0300

    second commit

---
+++ tut2.py
@@ -0,0 +1,36 @@
+import time
+from threading import Thread
+from multiprocessing import Process
+
+
+COUNT = 200000000
+SLEEP = 10
+
+
+def task0(n):
+    while n > 0:
+        n -= 1
+
+
+if __name__ == '__main__':
+    start = time.time()
+
+    # task0(COUNT)
+    # task0(COUNT)
+
+    # t1 = Thread(target=task0, args=(COUNT, ))
+    # t2 = Thread(target=task0, args=(COUNT, ))
+    # t1.start()
+    # t2.start()
+    # t1.join()
+    # t2.join()
+
+    p1 = Process(target=task0, args=(COUNT, ))
+    p2 = Process(target=task0, args=(COUNT, ))
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+
+    end = time.time()
+    print('Time taken in seconds -', end - start)
